8c0e35f0cb25accfb9ae7604438909c5769c607a
cschneider4711 <mail@christian-schneider.net>
2019-05-21 22:20:19 +0200

    added creation of required empty logs folder for local integration test

--- integration/start-integration.sh
+++ integration/start-integration.sh
@@ -2,8 +2,8 @@
 cp ../target/marathon.war web/tomcat-port-9090/webapps
 
 # ensure the application has all their technical requirements met (here for photo scanning and display)
-mkdir /root/marathonImages
-mkdir /root/marathonScripts
+mkdir -p /root/marathonImages
+mkdir -p /root/marathonScripts
 cp ../src/main/java/demo/util/default.png /root/marathonImages/default.png
 cp ../src/main/java/demo/antivirus/scanFile.sh /root/marathonScripts/scanFile.sh
 chmod 755 ../src/main/java/demo/antivirus/scanFile.sh
@@ -14,6 +14,7 @@ pwd
 ./startMarathonDB.sh &
 sleep 2
 cd ../../../
+mkdir -p web/tomcat-port-9090/logs
 cd web/tomcat-port-9090/bin/
 unset CATALINA_HOME
 pwd
